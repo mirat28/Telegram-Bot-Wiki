d306367de929b0c2b9221ce7d2cc3d0a68cf9633
mirat28 <105239814+mirat28@users.noreply.github.com>
2022-07-17 18:57:01 +0600

    Update main.py

--- main.py
+++ main.py
@@ -12,16 +12,12 @@ def getwiki(s):
         ny = wikipedia.page(s)
 
         wikitext = ny.content[:1000]
-        #
         wikimas = wikitext.split('.')
-
         wikimas = wikimas[:-1]
-
         wikitext2 = ''
 
         for x in wikimas:
             if not ('==' in x):
-
                 if (len((x.strip())) > 3):
                     wikitext2 = wikitext2 + x + '.'
             else:
@@ -30,7 +26,7 @@ def getwiki(s):
         wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
         wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
         wikitext2 = re.sub('\{[^\{\}]*\}', '', wikitext2)
-
+       
         return wikitext2
 
     except Exception as e:
@@ -47,4 +43,4 @@ def handle_text(message):
     bot.send_message(message.chat.id, getwiki(message.text))
 
 
-bot.polling(none_stop=True, interval=0)
+bot.polling(none_stop=True, interval=0) # Запускаем
